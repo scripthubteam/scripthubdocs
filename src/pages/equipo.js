e92c470285a992233154451e886b393d1deeeee7
Lauuu <elkozi116@gmail.com>
2019-11-26 10:11:59 -0300

    Añadido hipervinculos de github

--- src/pages/equipo.js
+++ src/pages/equipo.js
@@ -29,58 +29,68 @@ const useStyles = makeStyles(theme => ({
 
 const teams = [
     {
-      title: <>Lauuu</>,
+      title: <>Laauuu</>,
       imageUrl: 'https://avatars2.githubusercontent.com/u/33169908',
       info: "Miembro del departamento general.",
+      gitUser: "https://github.com/Laauuu",
     },
     {
       title: <>Devsaider</>,
       imageUrl: 'https://avatars0.githubusercontent.com/u/14142484',
       info: "Miembro del departamento general.",
+      gitUser: "https://github.com/MrDevsaider",
     },
     {
       title: <>Monspeet</>,
       imageUrl: 'https://avatars3.githubusercontent.com/u/36456999',
       info: "Miembro del departamento general.",
+      gitUser: "https://github.com/MagicNeko15",
     },
     {
       title: <>Tati</>,
       imageUrl: 'https://avatars0.githubusercontent.com/u/45135760',
       info: "Representante del departamento técnico.",
+      gitUser: "https://github.com/tati1206",
     },
     {
       title: <>Agate</>,
       imageUrl: 'https://avatars1.githubusercontent.com/u/42282680',
       info: "Miembro del departamento técnico.",
+      gitUser: "https://github.com/AgateDevelopment",
     },
     {
       title: <>Zhyntox</>,
       imageUrl: 'https://avatars1.githubusercontent.com/u/39054467',
       info: "Representante del departamento comunitario.",
+      gitUser: "https://github.com/Asynfur",
     },
     {
       title: <>GameBoy</>,
       imageUrl: "https://avatars2.githubusercontent.com/u/19270998",
       info: "Miembro del departamento comunitario.",
+      gitUser: "https://github.com/estebanDT30",
     },
     {
       title: <>Mon</>,
       imageUrl: 'https://avatars1.githubusercontent.com/u/54691517',
       info: "Miembro del departamento de moderación.",
+      gitUser: "https://github.com/wwmon",
     },
     {
       title: <>Axelito</>,
       imageUrl: 'img/logo-cord-raw.png',
       info: "Miembro del departamento de moderación.",
+      gitUser: "",
     },
     {
       title: <>iClu</>,
       imageUrl: 'https://avatars1.githubusercontent.com/u/35960254',
       info: "Miembro del departamento de moderación.",
+      gitUser: "https://github.com/BulzyKrown",
     }
   ];
 
-function Index({imageUrl, title, info}) {
+function Index({imageUrl, title, info, gitUser}) {
     const imgUrl = useBaseUrl(imageUrl);
     const classes = useStyles();
     
@@ -101,9 +111,9 @@ function Index({imageUrl, title, info}) {
             )}
             <h2 align="center">{title}</h2>
             <div className={styles.icons} align="center">
-              <IconContext.Provider value={{ size: "1.8em" }}>
-                  <FaGithub/>
-                  <FaTwitter/>
+              <IconContext.Provider value={{ size: "1.8em", color: "white" }}>
+                  <a href={gitUser}><FaGithub/></a>
+                  <a href="https://react-icons.netlify.com/#/"><FaTwitter/></a>
               </IconContext.Provider>
             </div>
       </div>
