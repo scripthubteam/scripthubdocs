219da5b5886df696b1856feaa7b0ea15aea3fbce
Lauuu <elkozi116@gmail.com>
2019-12-01 05:37:43 -0300

    Deploy website version based on 0f380e11d79b3a9d0a80e103266bab63b0bb7104

---
+++ 569478b1.6ab0a12b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[28],{163:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"git","name":"git","count":1,"permalink":"/blog/tags/git"}')}}]);
---
+++ f3967565.41033634.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[79],{212:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"android","name":"android","count":1,"permalink":"/blog/tags/android"}')}}]);
